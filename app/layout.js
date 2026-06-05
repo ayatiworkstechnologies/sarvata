3e7f3ecbc3668aeecf09ebb989d80d18af978057
rubankumarsankar <srirubankumar@gmail.com>
2026-06-05 16:20:02 +0530

    5/6/2026 update gtag

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Lexend, Mulish } from "next/font/google";
 import "./globals.css";
 import { Suspense } from "react";
+import Script from "next/script";
 
 import Header from "@/components/Navbar";
 import Footer from "@/components/Footer";
@@ -77,6 +78,16 @@ export default function RootLayout({ children }) {
           bg-background text-foreground
         `}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KSB89XSV"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+
         <ConsultationProvider>
           <Header />
           <Suspense fallback={<SarvataLoader />}>
@@ -87,6 +98,17 @@ export default function RootLayout({ children }) {
           {/* Single modal instance - driven by global ConsultationContext */}
           <ConsultationModalWrapper />
         </ConsultationProvider>
+
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-KSB89XSV');
+          `}
+        </Script>
       </body>
     </html>
   );
