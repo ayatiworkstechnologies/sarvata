10618b3b1b4dc04cc6d958d631e5aad3e4a001b3
harini0510 <hariniarumugam05@gmail.com>
2026-07-16 19:56:52 +0530

    16

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  /* config options here */
   reactCompiler: true,
-
-  output: "export",
-
   images: {
     qualities: [100, 75],
-    unoptimized: true, // Required for static export
   },
-
-  trailingSlash: true, // Optional: generates folder-based URLs
+  
 };
+
 
 export default nextConfig;
